OCA/product-pack

[REF] product_pack: exception packs totalized and non-detailed
Packs totalized and non-detailed ignores the product pack list price
when computing the price.

This commit is a hack to keep the behavior of the modules before
this refactor.

More information in the issue #169.

product_pack/models/product_product.py
@@ -41,7 +41,17 @@ class ProductProduct(models.Model):
         if "uom" in self._context:
             uom = self.env["uom.uom"].browse([self._context["uom"]])
         for product in packs:
-            list_price = product.price_compute("list_price", uom=uom).get(product.id)
+            # NOTE: This exception is to avoid adding the list price of the packs
+            # "totalized" and "non detailed". Should be removed to solve the issue #169.
+            if (
+                product.pack_type == "non_detailed"
+                or product.pack_component_price == "totalized"
+            ):
+                list_price = 0
+            else:
+                list_price = product.price_compute("list_price", uom=uom).get(
+                    product.id
+                )
             list_price += sum(
                 product.pack_line_ids._pack_line_price_compute(
                     "list_price", uom=uom

adhoc-cicd/oca-product-pack

product_pack/models/product_product.py
@@ -41,7 +41,17 @@ class ProductProduct(models.Model):
         if "uom" in self._context:
             uom = self.env["uom.uom"].browse([self._context["uom"]])
         for product in packs:
-            list_price = product.price_compute("list_price", uom=uom).get(product.id)
+            # NOTE: This exception is to avoid adding the list price of the packs
+            # "totalized" and "non detailed". Should be removed to solve the issue #169.
+            if (
+                product.pack_type == "non_detailed"
+                or product.pack_component_price == "totalized"
+            ):
+                list_price = 0
+            else:
+                list_price = product._price_compute("list_price", uom=uom).get(
+                    product.id
+                )
             list_price += sum(
                 product.pack_line_ids._pack_line_price_compute(
                     "list_price", uom=uom