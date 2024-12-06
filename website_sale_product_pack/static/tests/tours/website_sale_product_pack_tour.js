f936f6f891efe86f6a6e298edf8193ffca02ee30
Alexis Lopez <loa@adhoc.com.ar>
2024-12-06 16:01:27 -0300

    [MIG] website_sale_product_pack: Migration to 18.0

--- website_sale_product_pack/static/tests/tours/website_sale_product_pack_tour.js
+++ website_sale_product_pack/static/tests/tours/website_sale_product_pack_tour.js
@@ -3,10 +3,9 @@
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
 import {registry} from "@web/core/registry";
-import tourUtils from "@website_sale/js/tours/tour_utils";
+import * as tourUtils from "@website_sale/js/tours/tour_utils";
 
 registry.category("web_tour.tours").add("create_components_price_order_line", {
-    test: true,
     url: "/shop",
     steps: () => [
         ...tourUtils.searchProduct("Pack CPU (Detailed - Displayed Components Price)"),
@@ -14,17 +13,18 @@ registry.category("web_tour.tours").add("create_components_price_order_line", {
             content: "select Pack CPU (Detailed - Displayed Components Price)",
             trigger:
                 '.oe_product_cart:first a:contains("Pack CPU (Detailed - Displayed Components Price)")',
+            run: "click",
         },
         {
             content: "click on add to cart",
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
+            run: "click",
         },
         tourUtils.goToCart(),
     ],
 });
 
 registry.category("web_tour.tours").add("create_ignored_price_order_line", {
-    test: true,
     url: "/shop",
     steps: () => [
         ...tourUtils.searchProduct("Pack CPU (Detailed - Ignored Components Price)"),
@@ -32,17 +32,18 @@ registry.category("web_tour.tours").add("create_ignored_price_order_line", {
             content: "select Pack CPU (Detailed - Ignored Components Price)",
             trigger:
                 '.oe_product_cart:first a:contains("Pack CPU (Detailed - Ignored Components Price)")',
+            run: "click",
         },
         {
             content: "click on add to cart",
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
+            run: "click",
         },
         tourUtils.goToCart(),
     ],
 });
 
 registry.category("web_tour.tours").add("create_totalized_price_order_line", {
-    test: true,
     url: "/shop",
     steps: () => [
         ...tourUtils.searchProduct("Pack CPU (Detailed - Totalized Components Price)"),
@@ -50,17 +51,18 @@ registry.category("web_tour.tours").add("create_totalized_price_order_line", {
             content: "select Pack CPU (Detailed - Totalized Components Price)",
             trigger:
                 '.oe_product_cart:first a:contains("Pack CPU (Detailed - Totalized Components Price)")',
+            run: "click",
         },
         {
             content: "click on add to cart",
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
+            run: "click",
         },
         tourUtils.goToCart(),
     ],
 });
 
 registry.category("web_tour.tours").add("create_non_detailed_price_order_line", {
-    test: true,
     url: "/shop",
     steps: () => [
         ...tourUtils.searchProduct("Non Detailed - Totalized Components Price"),
@@ -68,17 +70,18 @@ registry.category("web_tour.tours").add("create_non_detailed_price_order_line", 
             content: "select Non Detailed - Totalized Components Price",
             trigger:
                 '.oe_product_cart:first a:contains("Non Detailed - Totalized Components Price")',
+            run: "click",
         },
         {
             content: "click on add to cart",
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
+            run: "click",
         },
         tourUtils.goToCart(),
     ],
 });
 
 registry.category("web_tour.tours").add("update_pack_qty", {
-    test: true,
     url: "/shop",
     steps: () => [
         ...tourUtils.searchProduct("Pack CPU (Detailed - Displayed Components Price)"),
