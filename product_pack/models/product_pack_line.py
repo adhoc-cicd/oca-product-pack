OCA/product-pack

[REF] product_pack: Price computation refactor
This commit improves the way the price of the packs is computed to
ensure it aligns with the behavior of normal products in Odoo.

Key improvements include:

1. Refactor Pack Price Computation:

Removed the inheritance from price_compute in favor of a clearer and
more comprehensible _get_product_price method within the Pricelist class

2. 'get_price' -> '_get_pack_line_price':

The get_price method on pack lines has been improved and renamed to
_get_pack_line_price. It is now capable of returning the price of the
 pack line for a specified pricelist.

3. List Price Calculation:

Enhanced the _compute_product_lst_price method to return the list price
of a pack. It now leverages a new method analogous to price_compute,
named _pack_line_price_compute, which can determine the prices of pack
components, including nested packs.

product_pack/models/product_pack_line.py
@@ -54,6 +54,35 @@ class ProductPackLine(models.Model):
                     )
                 pack_lines = pack_lines.mapped("product_id.pack_line_ids")
 
-    def get_price(self):
+    def _get_pack_line_price(self, pricelist, quantity, uom=None, date=False, **kwargs):
         self.ensure_one()
-        return self.product_id.lst_price * self.quantity
+        if self.product_id._is_pack_to_be_handled():
+            price = pricelist._get_product_price(
+                self.product_id, quantity, uom=uom, date=date, **kwargs
+            )
+        else:
+            price = pricelist._compute_price_rule(
+                self.product_id, quantity, uom=uom, date=date, **kwargs
+            )[self.product_id.id][0]
+        return price * self.quantity
+
+    def _pack_line_price_compute(
+        self, price_type, uom=False, currency=False, company=False, date=False
+    ):
+        packs, no_packs = self.product_id.split_pack_products()
+
+        pack_prices = {}
+        # If the component is a pack
+        for pack in packs:
+            pack_prices[pack.id] = pack.lst_price
+
+        # else
+        no_pack_prices = no_packs.price_compute(
+            price_type, uom, currency, company, date
+        )
+
+        prices = {**pack_prices, **no_pack_prices}
+        for line in self:
+            prices[line.product_id.id] *= line.quantity
+
+        return prices

adhoc-cicd/oca-product-pack

product_pack/models/product_pack_line.py
@@ -54,6 +54,35 @@ class ProductPackLine(models.Model):
                     )
                 pack_lines = pack_lines.mapped("product_id.pack_line_ids")
 
-    def get_price(self):
+    def _get_pack_line_price(self, pricelist, quantity, uom=None, date=False, **kwargs):
         self.ensure_one()
-        return self.product_id.lst_price * self.quantity
+        if self.product_id._is_pack_to_be_handled():
+            price = pricelist._get_product_price(
+                self.product_id, quantity, uom=uom, date=date, **kwargs
+            )
+        else:
+            price = pricelist._compute_price_rule(
+                self.product_id, quantity, uom=uom, date=date, **kwargs
+            )[self.product_id.id][0]
+        return price * self.quantity
+
+    def _pack_line_price_compute(
+        self, price_type, uom=False, currency=False, company=False, date=False
+    ):
+        packs, no_packs = self.product_id.split_pack_products()
+
+        pack_prices = {}
+        # If the component is a pack
+        for pack in packs:
+            pack_prices[pack.id] = pack.lst_price
+
+        # else
+        no_pack_prices = no_packs._price_compute(
+            price_type, uom, currency, company, date
+        )
+
+        prices = {**pack_prices, **no_pack_prices}
+        for line in self:
+            prices[line.product_id.id] *= line.quantity
+
+        return prices